e53295821a4f56d143bcc89b581888d24ab247f2
Manan Surana <59257437+suranamanan@users.noreply.github.com>
2020-06-19 13:39:08 -0400

    getByCategory in product controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,6 +2,14 @@ const Products = require('../models/product.model.js');
 
 exports.getByCategory = function(req, res){
 
-  Products.getByCategory()
+  Products.getByCategory(req.params.category, function(error, data){
+    if(error){
+      res.status(500).send({message: "Internal Server Error when searching by Category"
+        + req.params.category})
+    }else{
+      //Name, id, category, price, quantity JSON
+      res.send(data);
+    }
+  });
 
 }
